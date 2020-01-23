199bafbfac9ea87d11a13b5a02e3ff86a94c34f8
Willy Borja <bor_williamalexander@hotmail.com>
2020-01-22 19:20:45 -0500

    Implementing Jose's changes

--- app/src/main/java/com/appTec/RegistrateApp/view/activities/bottomNavigationUi/home/HomeFragment.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/activities/bottomNavigationUi/home/HomeFragment.java
@@ -5,22 +5,105 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.appTec.RegistrateApp.R;
+import com.appTec.RegistrateApp.view.activities.generic.DayViewContainer;
+import com.appTec.RegistrateApp.view.activities.generic.MonthHeaderViewContainer;
+import com.jakewharton.threetenabp.AndroidThreeTen;
+import com.kizitonwose.calendarview.CalendarView;
+import com.kizitonwose.calendarview.model.CalendarDay;
+import com.kizitonwose.calendarview.model.CalendarMonth;
+import com.kizitonwose.calendarview.ui.DayBinder;
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder;
+import com.kizitonwose.calendarview.ui.ViewContainer;
+
+import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.YearMonth;
+import org.threeten.bp.format.TextStyle;
+import org.threeten.bp.temporal.WeekFields;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class HomeFragment extends Fragment {
 
+    public static final Locale LOCALE_ES = Locale.forLanguageTag("es-419");
+
+    @BindView(R.id.timer_text) TextView timerText;
+    @BindView(R.id.calendarView) CalendarView calendarView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        AndroidThreeTen.init(getContext());
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, root);
+
+        // Calendar
+        calendarView.setDayBinder(new DayBinder<DayViewContainer>() {
+            @NonNull
+            @Override
+            public DayViewContainer create(@NonNull View view) {
+                return new DayViewContainer(view);
+            }
+
+            @Override
+            public void bind(@NonNull DayViewContainer container, @NonNull CalendarDay calendarDay) {
+                container.getDayText().setText(calendarDay.getDate().getDayOfMonth() + "");
+                String dayName =
+                        calendarDay.getDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, LOCALE_ES);
+                container.getDayNameText().setText(dayName);
+            }
+        });
+        calendarView.setMonthHeaderBinder(new MonthHeaderFooterBinder<MonthHeaderViewContainer>() {
+            @NonNull
+            @Override
+            public MonthHeaderViewContainer create(@NonNull View view) {
+                return new MonthHeaderViewContainer(view);
+            }
+
+            @Override
+            public void bind(@NonNull MonthHeaderViewContainer container, @NonNull CalendarMonth calendarMonth) {
+                String month =
+                        calendarMonth.getYearMonth().getMonth().getDisplayName(TextStyle.FULL, LOCALE_ES);
+                String monthYearTxt = String.format("%s de %d", month, calendarMonth.getYear());
+                container.getMonthText().setText(monthYearTxt);
+                // Week scroll buttons
+                container.getLeftMonthButton().setOnClickListener(view -> {
+                    Toast.makeText(getContext(), "<-", Toast.LENGTH_SHORT).show();
+                });
+            }
+        });
+        YearMonth currentMonth = YearMonth.now();
+        YearMonth firstMonth = currentMonth.minusMonths(10);
+        YearMonth lastMonth = currentMonth.plusMonths(10);
+        DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek);
+        calendarView.scrollToMonth(currentMonth);
+
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+            }
+        });
         return root;
+    }
+
+    @OnClick(R.id.start_timer_btn)
+    public void startTimer(View view) {
+        YearMonth currentMonth = YearMonth.now();
+        YearMonth firstMonth = currentMonth.plusMonths(1);
+        calendarView.smoothScrollToMonth(firstMonth);
+        Toast.makeText(getContext(), "o/", Toast.LENGTH_SHORT).show();
     }
 }
