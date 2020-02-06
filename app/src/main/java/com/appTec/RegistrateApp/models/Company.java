e71ec3941ebbb437fcf0fe94bca075be5537244f
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2020-02-06 15:21:39 -0500

    Added home panel business logic

--- app/src/main/java/com/appTec/RegistrateApp/models/Company.java
+++ app/src/main/java/com/appTec/RegistrateApp/models/Company.java
@@ -8,7 +8,7 @@ public class Company implements Serializable {
     private String name;
     private double latitude;
     private double longitude;
-    private double radius;
+    private float radius;
 
     public String getName() {
         return name;
@@ -34,11 +34,11 @@ public class Company implements Serializable {
         this.longitude = longitude;
     }
 
-    public double getRadius() {
+    public float getRadius() {
         return radius;
     }
 
-    public void setRadius(double radius) {
+    public void setRadius(float radius) {
         this.radius = radius;
     }
 }
