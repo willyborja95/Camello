d01baf945d70db8c075ead9c926f9eb74e09c8de
renatojobal <renatojobal@gmail.com>
2020-04-06 20:41:44 -0500

    Adding docstring

---
+++ app/src/main/java/com/appTec/RegistrateApp/models/Notification.java
@@ -0,0 +1,9 @@
+package com.appTec.RegistrateApp.models;
+
+public class Notification {
+    /*
+    * Model of a notification.
+    *
+    * This will be used for the list adapter attached with the recycler view.
+    * */
+}
--- app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
@@ -1,4 +1,7 @@
 package com.appTec.RegistrateApp.view.adapters;
 
 public class NotificationsListAdapter {
+    /*
+    * This class will be used to attach the card view of notification to the recycler view.
+    * */
 }
