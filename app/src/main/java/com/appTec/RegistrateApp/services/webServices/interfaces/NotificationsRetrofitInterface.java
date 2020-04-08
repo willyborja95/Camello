17f34c4a1bb3c055b71241a260c648709bcaf17e
renatojobal <renatojobal@gmail.com>
2020-04-07 20:04:13 -0500

    Created interface and method to get all notifications

--- app/src/main/java/com/appTec/RegistrateApp/services/webServices/interfaces/NotificationsRetrofitInterface.java
+++ app/src/main/java/com/appTec/RegistrateApp/services/webServices/interfaces/NotificationsRetrofitInterface.java
@@ -1,4 +1,16 @@
 package com.appTec.RegistrateApp.services.webServices.interfaces;
 
-public interface NotificationsRetrofitInterfaces {
+import com.appTec.RegistrateApp.util.Constants;
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+public interface NotificationsRetrofitInterface {
+
+    // Get all the notifications
+    @GET(Constants.NOTIFICATIONS_URL)
+    Call<JsonObject> get(@Header("authorization") String token, @Path("userId") int userId);
 }
