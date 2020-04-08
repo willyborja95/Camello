1a9ee71f3b9d22ab402686a226bcf380aad588c2
renatojobal <renatojobal@gmail.com>
2020-04-07 20:03:36 -0500

    Using constant for base url

--- app/src/main/java/com/appTec/RegistrateApp/services/webServices/ApiClient.java
+++ app/src/main/java/com/appTec/RegistrateApp/services/webServices/ApiClient.java
@@ -1,5 +1,7 @@
 package com.appTec.RegistrateApp.services.webServices;
 
+import com.appTec.RegistrateApp.util.Constants;
+
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -7,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "https://registrateapp.com.ec/api/";
+
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
@@ -20,7 +22,7 @@ public class ApiClient {
                     .build();
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Constants.BASE_URL)
                     .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
