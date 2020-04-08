2760603a5f5760c896fd57ea6b15f6a34a7710b1
renatojobal <renatojobal@gmail.com>
2020-04-07 19:43:37 -0500

    Added docstring

--- app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
@@ -16,10 +16,15 @@ import java.util.ArrayList;
 
 public class NotificationsListAdapter extends BaseAdapter {
 
+    // Attributes
     Context context;
     ArrayList<Notification> notifications;
 
+
     public NotificationsListAdapter(Context context, ArrayList<Notification> notifications ){
+        /*
+        * Constructor
+        * */
         this.context = context;
         this.notifications = notifications;
     }
@@ -39,6 +44,7 @@ public class NotificationsListAdapter extends BaseAdapter {
         TextView sentDate = convertView.findViewById(R.id.notification_sent_date);
 
         title.setText(notification.getTitle());
+        sentDate.setText(notification.getSentDate().toString());
 
         return convertView;
     }
@@ -58,8 +64,6 @@ public class NotificationsListAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return notifications.indexOf(position);
     }
-
-
 
 
 
