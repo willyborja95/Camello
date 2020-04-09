7d6dd8f8bdf11795cd3c90d64c053394072a3e27
renatojobal <renatojobal@gmail.com>
2020-04-09 01:28:27 -0500

    Created the intereface for the login view

---
+++ app/src/main/java/com/appTec/RegistrateApp/view/LoginActivityView.java
@@ -0,0 +1,10 @@
+package com.appTec.RegistrateApp.view;
+
+public interface LoginActivityView {
+    /**
+    * The interface for the view.
+    * */
+
+    // ToDo: Pass the LoginActivity methods here
+
+}
