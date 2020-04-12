f2347775d2a0a4fb6180f57885beb9c94acae2f2
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Interface created for can control the view in the MVP pattern"
    This reverts commit 16957da118474be2a8f34d1d6d57572e726abd51.

--- app/src/main/java/com/appTec/RegistrateApp/view/activities/bottomNavigationUi/notifications/NotificationView.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/activities/bottomNavigationUi/notifications/NotificationView.java
@@ -1,17 +1,4 @@
 package com.appTec.RegistrateApp.view.activities.bottomNavigationUi.notifications;
 
-import com.appTec.RegistrateApp.models.Notification;
-
-import java.util.ArrayList;
-
 public interface NotificationView {
-    /*
-    * Notification View interface
-    * */
-
-
-    void getNotifications(); // Presenter
-    void showNotifications(ArrayList<Notification> notifications);
-
-
 }
