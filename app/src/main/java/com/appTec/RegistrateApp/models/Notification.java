92d6df4f04f5c797fc8c875208a2ca91cbc34858
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Adding docstring"
    This reverts commit ee2a2b766ad294c05c4d742ed40beefa261fd74a.

--- app/src/main/java/com/appTec/RegistrateApp/models/Notification.java
+++
@@ -1,9 +0,0 @@
-package com.appTec.RegistrateApp.models;
-
-public class Notification {
-    /*
-    * Model of a notification.
-    *
-    * This will be used for the list adapter attached with the recycler view.
-    * */
-}
--- app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
@@ -1,7 +1,4 @@
 package com.appTec.RegistrateApp.view.adapters;
 
 public class NotificationsListAdapter {
-    /*
-    * This class will be used to attach the card view of notification to the recycler view.
-    * */
 }
