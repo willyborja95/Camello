e5f51a4c41b3c724bb923f2dc3126d353bd7679a
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Moved docstring"
    This reverts commit f74aacbc8eaa1b6db778a0e9a4b5d3213ff2b272.

--- app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
@@ -1,20 +1,7 @@
 package com.appTec.RegistrateApp.view.adapters;
 
-import android.view.View;
-
-import androidx.recyclerview.widget.RecyclerView;
-
 public class NotificationsListAdapter {
     /*
-     * This class will be used to attach the card view of notification to the recycler view.
-     * */
-
-
-    public class NotificationViewHolder extends RecyclerView.ViewHolder {
-
-        public NotificationViewHolder(View itemView) {
-            super(itemView);
-        }
-
-    }
+    * This class will be used to attach the card view of notification to the recycler view.
+    * */
 }
