8937c8ab7db2e9828a58ceb714f35bf95e098633
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Refactoring the login activity. Not completed yet."
    This reverts commit c317931da451cd6b08a7533abfeaf1044f5776a7.

--- app/src/main/java/com/appTec/RegistrateApp/presenter/LoginPresenter.java
+++ app/src/main/java/com/appTec/RegistrateApp/presenter/LoginPresenter.java
@@ -1,14 +1,4 @@
 package com.appTec.RegistrateApp.presenter;
 
 public interface LoginPresenter {
-    /**
-     * Interface for login presenter
-     */
-
-    void getInitialData();  // From the view
-    void loadInitialData(); // TO the interactor
-
-
-
-
 }
