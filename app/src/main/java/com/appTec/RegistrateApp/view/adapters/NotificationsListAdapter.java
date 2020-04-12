70bedcecbf34e23f596ca0cf8e095275efa4b2ea
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Extends from a BaseAdapter"
    This reverts commit b0cc93124176d5fa4c3cfaba8b005691334cb2e8.

--- app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
+++ app/src/main/java/com/appTec/RegistrateApp/view/adapters/NotificationsListAdapter.java
@@ -1,66 +1,20 @@
 package com.appTec.RegistrateApp.view.adapters;
 
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.appTec.RegistrateApp.R;
-import com.appTec.RegistrateApp.models.Notification;
+public class NotificationsListAdapter {
+    /*
+     * This class will be used to attach the card view of notification to the recycler view.
+     * */
 
-import java.util.ArrayList;
 
-public class NotificationsListAdapter extends BaseAdapter {
+    public class NotificationViewHolder extends RecyclerView.ViewHolder {
 
-    Context context;
-    ArrayList<Notification> notifications;
+        public NotificationViewHolder(View itemView) {
+            super(itemView);
+        }
 
-    public NotificationsListAdapter(Context context, ArrayList<Notification> notifications ){
-        this.context = context;
-        this.notifications = notifications;
     }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        /*
-         * This method will be used to attach the card view of notification to the list view.
-         * */
-        convertView = LayoutInflater.from(context).inflate(R.layout.notifications_card_view, null);
-
-        // Getting the object by the position
-        Notification notification = getItem(position);
-
-        // Binding UI elements
-        TextView title = convertView.findViewById(R.id.notification_title);
-        TextView sentDate = convertView.findViewById(R.id.notification_sent_date);
-
-        title.setText(notification.getTitle());
-
-        return convertView;
-    }
-
-
-    @Override
-    public int getCount() {
-        return notifications.size();
-    }
-
-    @Override
-    public Notification getItem(int position) {
-        return notifications.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return notifications.indexOf(position);
-    }
-
-
-
-
-
 }
