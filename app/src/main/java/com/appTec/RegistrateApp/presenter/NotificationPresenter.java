32873d0598f3a2057376eb3b948939ccfb6dab2a
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Created communication with the interactor, but i don't know how it works with hte view because it is a fragment"
    This reverts commit 77ef41c151ebadf739a04b19c8a9ac1ae2cde2b8.

--- app/src/main/java/com/appTec/RegistrateApp/presenter/NotificationPresenter.java
+++ app/src/main/java/com/appTec/RegistrateApp/presenter/NotificationPresenter.java
@@ -1,11 +1,4 @@
 package com.appTec.RegistrateApp.presenter;
 
-import android.view.View;
-
-public interface NotificationPresenter {
-
-    void getNotifications(); // To interactor
-    void showNotifications(); // To view
-
-
+public class NotificationPresenter {
 }
