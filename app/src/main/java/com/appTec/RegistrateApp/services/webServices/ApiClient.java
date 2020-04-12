c5704ba3cccc97b41f877c49a2e5b30ff35f2f6d
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Adding token stuff"
    This reverts commit 604becb6d7981cbf7b3944539e1bef017cc9c683.

--- app/src/main/java/com/appTec/RegistrateApp/services/webServices/ApiClient.java
+++ app/src/main/java/com/appTec/RegistrateApp/services/webServices/ApiClient.java
@@ -1,8 +1,5 @@
 package com.appTec.RegistrateApp.services.webServices;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
 import com.appTec.RegistrateApp.util.Constants;
 
 import java.util.concurrent.TimeUnit;
@@ -14,8 +11,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     private static Retrofit retrofit = null;
-
-    private static String token = null;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
@@ -34,26 +29,4 @@ public class ApiClient {
         }
         return retrofit;
     }
-
-
-
-    public static String getToken(){
-        /*
-        * Get the token previously saved without context
-        * */
-        return ApiClient.token;
-    }
-
-
-    public static void setToken(String value, Context context){
-        /*
-        * Save the token into shared preferences in private mode
-        * */
-        SharedPreferences sharedPref = context.getSharedPreferences(
-                Constants.SHARED_PREFERENCES_GLOBAL, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(Constants.USER_TOKEN, value);
-        editor.commit();
-    }
-
 }
