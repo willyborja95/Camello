22300d0c30d8e26ab51ebbce9516830599dd3c03
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Modified view method"
    This reverts commit bc6f1f20c30b96505e32ed8ac3cc2f366edbea87.

--- app/src/main/java/com/appTec/RegistrateApp/presenter/NotificationPresenter.java
+++ app/src/main/java/com/appTec/RegistrateApp/presenter/NotificationPresenter.java
@@ -2,14 +2,10 @@ package com.appTec.RegistrateApp.presenter;
 
 import android.view.View;
 
-import com.appTec.RegistrateApp.models.Notification;
-
-import java.util.ArrayList;
-
 public interface NotificationPresenter {
 
     void getNotifications(); // To interactor
-    void showNotifications(ArrayList<Notification> notifications); // To view
+    void showNotifications(); // To view
 
 
 }
