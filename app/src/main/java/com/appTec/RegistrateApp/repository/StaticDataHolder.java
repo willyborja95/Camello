3cee94997138fa2089ba522c5addb2999756e3fe
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Refactoring name StaticDataHolder to Static Data"
    This reverts commit 15f2d08fbbf1048a97a7507d1045c51e5007d20f.

---
+++ app/src/main/java/com/appTec/RegistrateApp/repository/StaticDataHolder.java
@@ -0,0 +1,10 @@
+package com.appTec.RegistrateApp.repository;
+
+public class StaticDataHolder {
+    /**
+     * This is a class when we can hold data for the app. In that way it can be access from anywhere of the app.
+     */
+
+    //
+
+}
