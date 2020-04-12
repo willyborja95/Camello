f0ab3ff8d506024455093ed657284058cd0a1b00
Renato Johao Balcázar Loaiza <renatojobal@gmail.com>
2020-04-12 10:50:41 -0500

    Revert "Created java class fragment for notifications"
    This reverts commit 57f5ae832dba6df1d66e676dfc294120134cdcc4.

--- app/src/main/java/com/appTec/RegistrateApp/view/activities/bottomNavigationUi/notifications/NotificationsFragment.java
+++
@@ -1,6 +0,0 @@
-package com.appTec.RegistrateApp.view.activities.bottomNavigationUi.notifications;
-
-import androidx.fragment.app.Fragment;
-
-public class NotificationsFragment extends Fragment {
-}
