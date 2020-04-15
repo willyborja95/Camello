6691c89106efc01b2894a8f35574b73c99198f6a
renatojobal <renatojobal@gmail.com>
2020-04-15 13:30:48 -0500

    Added usefull constants

--- app/src/main/java/com/appTec/RegistrateApp/util/Constants.java
+++ app/src/main/java/com/appTec/RegistrateApp/util/Constants.java
@@ -15,19 +15,24 @@ public class Constants {
     public static final String UPDATING_CHANGES = "Actualizando datos";
 
 
-    public static final String SHARED_PREFERENCES_GLOBAL = "com.appTec.RegistrateApp.PREFERENCES_FILE_KEY";
-    public static final String CURRENT_STATE = "CURRENT_STATE";
-    public static final String LOGIN_USER_STATE = "LOGGED";
+    // Shared preferences key words values
+    public static final String SHARED_PREFERENCES_GLOBAL = "com.appTec.RegistrateApp.PREFERENCES_FILE_KEY"; // Storage
+    public static final String CURRENT_STATE = "CURRENT_STATE";                                     //
+    public static final String LOGIN_USER_STATE = "LOGGED";                                         //
 
-    public static final String STATE_WORKING = "WORKING";
-    public static final String STATE_NOT_WORKING = "NOT WORKING";
+    public static final String STATE_WORKING = "WORKING";                                           //
+    public static final String STATE_NOT_WORKING = "NOT WORKING";                                   //
 
-    public static final String LOGGED_USER = "LOGGED";
-    public static final String NOT_LOGGED_USER = "NOT LOGGED";
+    public static final String LOGGED_USER = "LOGGED";                                              //
+    public static final String NOT_LOGGED_USER = "NOT LOGGED";                                      //
 
-    public static final String USER_TOKEN = "USERTOKEN";
+    public static final String USER_TOKEN = "USERTOKEN";                                            //
 
-    public static final String LAST_EXIT_TIME = "LAST_EXIT_TIME";
+    public static final String LAST_EXIT_TIME = "LAST_EXIT_TIME";                                   //
+
+    public static final String APP_IS_RUNNING_BY_FIRST_TIME = "IS_FIRST_TIME";                      // Storage a boolean
+    public static final String USER_ACCESS_TOKEN = "ACCESS_TOKEN";                                  // Storage a string
+    public static final String USER_REFRESH_TOKEN = "REFRESH_TOKEN";                                // Storage a string
 
 
 
