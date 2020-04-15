198c65abf8a682e8b322d9fc4b1e2fcda58a1f2e
renatojobal <renatojobal@gmail.com>
2020-04-15 11:59:42 -0500

    Refactored method to access the app context

--- app/src/main/java/com/appTec/RegistrateApp/App.java
+++ app/src/main/java/com/appTec/RegistrateApp/App.java
@@ -11,24 +11,21 @@ public class App extends Application {
      * This class is used oly for return an access to the application context from every part of the app.
      */
 
-    private static App instance;
+    private static Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        App.context = getApplicationContext();
     }
 
-    public App() {
-        instance = this;
-    }
 
     public static Context getContext(){
         /**
          * Be careful using this method only when you need the global application context. Do not use
          * it when you need a view context.
          */
-        return instance;
+        return App.context;
     }
 
 
