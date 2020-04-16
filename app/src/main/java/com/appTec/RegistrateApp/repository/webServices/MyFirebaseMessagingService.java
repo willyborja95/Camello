31ec9fe26d04600bb65ca5c6fca937ce8541c50f
renatojobal <renatojobal@gmail.com>
2020-04-16 11:52:29 -0500

    The FMC is integrated.

---
+++ app/src/main/java/com/appTec/RegistrateApp/repository/webServices/MyFirebaseMessagingService.java
@@ -0,0 +1,60 @@
+package com.appTec.RegistrateApp.repository.webServices;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    /**
+     * Class for manage the FMC
+     */
+
+    private final String TAG = "FMC";
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+// ...
+
+        // ToDo: Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                // ToDo: scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                // ToDo: handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+
+    }
+
+//
+//    @Override
+//    public void onNewToken(@NonNull String token) {
+//        Log.d(TAG, "Refreshed token: " + token);
+//
+//        // If you want to send messages to this application instance or
+//        // manage this apps subscriptions on the server side, send the
+//        // Instance ID token to your app server.
+//        // Todo: sendRegistrationToServer(token);
+//    }
+}
