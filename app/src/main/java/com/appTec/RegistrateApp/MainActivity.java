3acf252b4c9af2e1a640379b722c4b61ccb39c6d
renatojobal <renatojobal@gmail.com>
2020-04-24 11:48:26 -0500

    Binding UI elements

--- app/src/main/java/com/appTec/RegistrateApp/MainActivity.java
+++ app/src/main/java/com/appTec/RegistrateApp/MainActivity.java
@@ -1,15 +1,34 @@
 package com.appTec.RegistrateApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MaBinActivity extends AppCompatActivity {
+
+    // UI elements (?: Use data binding)
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.slide_nav_view)
+//        navController = findNavController(R.id.nav_host_fragment)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
     }
 
 
