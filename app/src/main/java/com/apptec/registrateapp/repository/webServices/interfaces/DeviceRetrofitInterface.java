dfb949690fa0e6e31182ddedcf69fd0a02b6cc36
renatojobal <renatojobal@gmail.com>
2020-04-28 09:10:03 -0500

    Created call function in retrofit interface

--- app/src/main/java/com/apptec/registrateapp/repository/webServices/interfaces/DeviceRetrofitInterface.java
+++ app/src/main/java/com/apptec/registrateapp/repository/webServices/interfaces/DeviceRetrofitInterface.java
@@ -1,6 +1,7 @@
 package com.apptec.registrateapp.repository.webServices.interfaces;
 
 import com.apptec.registrateapp.models.Device;
+import com.apptec.registrateapp.util.Constants;
 import com.google.gson.JsonObject;
 
 import retrofit2.Call;
@@ -11,9 +12,28 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface DeviceRetrofitInterface {
+
+
     @POST("dispositivo/")
     Call<JsonObject> post(@Header("authorization") String token, @Body Device device);
 
     @GET("dispositivo/empleado/{userId}")
     Call<JsonObject> get(@Header("authorization") String token, @Path("userId") int userId);
+
+
+    /**
+     * Permite consultar si el dispositivo actual del empleado.
+     *
+     * Se debe especificar el identificador del dispositivo.
+     *
+     * https://registrateapp-staging/api/employees/device?identifier=IMEI
+     * HEADERS
+     * Authorization
+     * PARAMS
+     * identifier IMEI
+     */
+    @GET("/api/employee/device?identifier={indentifier}")
+    Call<JsonObject> get(
+            @Header(Constants.AUTHORIZATION_HEADER) String accessToken,
+            @Path("identifier") String IMEI);
 }
