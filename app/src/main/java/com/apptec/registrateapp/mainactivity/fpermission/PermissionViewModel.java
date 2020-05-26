c5af34e37c8db69c46d172e7c0779600963ecefa
renatojobal <renatojobal@gmail.com>
2020-05-26 17:08:27 -0500

    Using a view model and binding data into the permission fragment

---
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fpermission/PermissionViewModel.java
@@ -0,0 +1,44 @@
+package com.apptec.registrateapp.mainactivity.fpermission;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import timber.log.Timber;
+
+public class PermissionViewModel extends ViewModel {
+    /**
+     * Singular view model fro the fragment actions
+     * <p>
+     * The permission list will be on the main presenter for not be getting the data each time that the fragment onResume
+     */
+
+    // Variables that observe the fragment view from the get methods
+    private MutableLiveData<Boolean> _addNewPermission = new MutableLiveData<Boolean>();
+
+    public PermissionViewModel() {
+        // Constructor
+        Timber.w("Constructor used");
+    }
+
+    // Expose the data
+    public LiveData<Boolean> addNewPermission() {
+        /**
+         * This method will be observed by the fragment viewe
+         */
+        return _addNewPermission;
+    }
+
+
+    // This function is called by the layout
+    public void onAddNewPermission(Boolean value) {
+        Timber.d("onAddNewPermission clicked");
+        this._addNewPermission.setValue(value);
+    }
+
+    @Override
+    protected void onCleared() {
+        Timber.w("View model cleaned");
+        super.onCleared();
+    }
+}
--- app/src/main/java/com/apptec/registrateapp/mainactivity/fpermission/ui/PermissionFragment.java
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fpermission/ui/PermissionFragment.java
@@ -1,24 +1,26 @@
 package com.apptec.registrateapp.mainactivity.fpermission.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.apptec.registrateapp.R;
+import com.apptec.registrateapp.databinding.FragmentPermissionBinding;
 import com.apptec.registrateapp.mainactivity.MainViewModel;
+import com.apptec.registrateapp.mainactivity.fpermission.PermissionViewModel;
 import com.apptec.registrateapp.models.PermissionModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
+
+import timber.log.Timber;
 
 public class PermissionFragment extends Fragment {
     /**
@@ -29,63 +31,66 @@ public class PermissionFragment extends Fragment {
 
     // Instance of ViewModel
     private MainViewModel mainViewModel;
+    private PermissionViewModel permissionViewModel;
+
+    FragmentPermissionBinding binding;
 
     // Ui elements
-    FloatingActionButton floatingActionButton;
-    ListView permissionsListView;
+
+
     PermissionListAdapter permissionListAdapter;
+
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);                    // Getting the view model
+        mainViewModel = ViewModelProviders.of(getActivity()).get(MainViewModel.class);                    // Getting the main view model
+        permissionViewModel = ViewModelProviders.of(this).get(PermissionViewModel.class);        // Getting the view model exclusive of this fragment
+
         mainViewModel.setActiveFragmentName(getString(R.string.permissions_fragment_title));
+
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_permission, container, false);
-        floatingActionButton = view.findViewById(R.id.add_permission_floating_button);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_permission, container, false);
+        binding.setPermissionViewModel(permissionViewModel);
+
+
         // TODO: Observe the mPermissionList
         mainViewModel.getPermissionsList().observe(this, new Observer<List<PermissionModel>>() {
             @Override
             public void onChanged(List<PermissionModel> permissionModels) {
                 // TODO
+                if (permissionModels.size() == 0) {
+                    Timber.d("No permissions found");
+
+                }
 
             }
         });
 
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        permissionViewModel.addNewPermission().observe(this, new Observer<Boolean>() {
             @Override
-            public void onClick(View v) {
-                /**
-                 * Open the dialog fragment to add a permission
-                 */
-                Log.d(TAG, "Open dialog");
+            public void onChanged(Boolean aBoolean) {
+                if (aBoolean) {
+                    /**
+                     * Open the dialog fragment to add a permission
+                     */
+                    Timber.d("Open dialog");
 
-                DialogPermission dialogPermission = new DialogPermission();
-                dialogPermission.show(getFragmentManager(), TAG);
-
-
-            }
-        });
+                    DialogPermission dialogPermission = new DialogPermission();
+                    dialogPermission.show(getFragmentManager(), TAG);
 
 
-        // When the user scroll down, then syn the permissions
-        view.setOnScrollChangeListener(new View.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                if (scrollX - oldScrollX > 1) {
-                    // If scroll down
-                    mainViewModel.syncPermissions();
                 }
             }
         });
 
-        return view;
+        return binding.getRoot();
     }
 
 }
