5acc18dd697f2fa5219848dce4727a8510b79073
renatojobal <renatojobal@gmail.com>
2020-06-12 18:16:50 -0500

    Logging with Timber

--- app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/CustomFirebaseMessagingService.java
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/CustomFirebaseMessagingService.java
@@ -1,8 +1,6 @@
 package com.apptec.registrateapp.mainactivity.fnotification;
 
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.apptec.registrateapp.models.NotificationModel;
@@ -15,41 +13,43 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Date;
 import java.util.Map;
 
+import timber.log.Timber;
+
 public class CustomFirebaseMessagingService extends FirebaseMessagingService {
     /**
      * Class for manage the FCM
      */
 
-    private final String TAG = "FCM";
+
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
 
         // Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "New message from: " + remoteMessage.getFrom());
+        Timber.d("New message from: " + remoteMessage.getFrom());
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData().getClass());
-            Log.d(TAG, "Message data: " + remoteMessage.getData());
+            Timber.d("Message data payload: " + remoteMessage.getData().getClass());
+            Timber.d("Message data: " + remoteMessage.getData());
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            Log.d(TAG, "Notification title: " + remoteMessage.getNotification().getTitle());
+            Timber.d("Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Timber.d("Notification title: " + remoteMessage.getNotification().getTitle());
 
             String title = remoteMessage.getNotification().getTitle();
             String text = remoteMessage.getNotification().getBody();
             Map map = remoteMessage.getData();
 
-            Log.d(TAG, "expirationDate: " + map.get("expirationDate").toString());
-            Log.d(TAG, "sentDate" + map.get("sentDate").toString());
+            Timber.d("expirationDate: " + map.get("expirationDate").toString());
+            Timber.d("sentDate" + map.get("sentDate").toString());
 
 
-            Date expirationDate =  new Date(Long.parseLong(map.get("expirationDate").toString())*1000);
-            Date sentDate = new Date(Long.parseLong(map.get("sentDate").toString())* 1000);
+            Date expirationDate = new Date(Long.parseLong(map.get("expirationDate").toString()) * 1000);
+            Date sentDate = new Date(Long.parseLong(map.get("sentDate").toString()) * 1000);
 
             NotificationModel notification = new NotificationModel(title, text, expirationDate, sentDate);
 
@@ -74,7 +74,7 @@ public class CustomFirebaseMessagingService extends FirebaseMessagingService {
          * This method is called when the data of the app is deleted by the user from the settings. So
          * the server could handle a token actualization.
          */
-        Log.d(TAG, "New token generate by firebase: " + token);
+        Timber.d("New token generate by firebase: " + token);
 
         SharedPreferencesHelper.putStringValue(Constants.FIREBASE_TOKEN, token); // Saving on share preferences to use later
 
