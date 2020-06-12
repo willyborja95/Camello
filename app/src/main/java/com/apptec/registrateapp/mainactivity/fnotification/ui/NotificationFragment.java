ccd87ce38d94a3c54ceed3a5fdce62cd34f5608c
renatojobal <renatojobal@gmail.com>
2020-06-12 18:35:24 -0500

    Replaced findViewById for data binding

--- app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/ui/NotificationFragment.java
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/ui/NotificationFragment.java
@@ -1,15 +1,14 @@
 package com.apptec.registrateapp.mainactivity.fnotification.ui;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -32,7 +31,6 @@ public class NotificationFragment extends Fragment {
 
 
     //UI elements
-    private ListView notificationsListView;
     private NotificationListAdapter notificationListAdapter;
 
 
@@ -66,20 +64,20 @@ public class NotificationFragment extends Fragment {
          * Create an onItemClickListener to show a dialog about each notification when is pressed.
          */
 
-        View view = inflater.inflate(R.layout.fragment_notification, container, false);
-        notificationsListView = view.findViewById(R.id.notification_list_view);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notification, container, false);
+
 
         // Observing the view model mNotification
         notificationListAdapter = new NotificationListAdapter(App.getContext(), mainViewModel.getNotifications());
         mainViewModel.getNotifications().observe(getActivity(), new Observer<List<NotificationModel>>() {
             @Override
             public void onChanged(List<NotificationModel> notifications) {
-                notificationsListView.setAdapter(notificationListAdapter);
+                binding.notificationListView.setAdapter(notificationListAdapter);
             }
         });
 
         // When clicked show a dialog with more information
-        notificationsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        binding.notificationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 /**
@@ -92,31 +90,11 @@ public class NotificationFragment extends Fragment {
             }
         });
 
-        return view;
+        return binding.getRoot();
     }
 
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        /**
-         * This method could be use for show a message that there are not new notifications.
-         * Or maybe the on change Live Data into the observer.
-         */
 
-        // Linking UI elements
-
-        notificationsListView = view.findViewById(R.id.notification_list_view);
-        // notificationTextView = view.findViewById(R.id.notification_text_view);
-
-        // showNotNewNotificationsMessage();
-    }
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-
-    }
 
 
 }
