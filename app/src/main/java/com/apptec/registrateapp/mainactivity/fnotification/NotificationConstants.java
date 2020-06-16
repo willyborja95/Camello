725b0979eef59fd57afe0817c58db7704acab111
renatojobal <renatojobal@gmail.com>
2020-06-16 08:53:18 -0500

    Created channel constants

---
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/NotificationConstants.java
@@ -0,0 +1,17 @@
+package com.apptec.registrateapp.mainactivity.fnotification;
+
+import android.app.NotificationManager;
+
+/**
+ * Class for have the constants about the Notification module
+ */
+public class NotificationConstants {
+
+
+    public static final String MESSAGES_CHANNEL_ID = "messages";
+    public static final int MESSAGES_IMPORTANCE = NotificationManager.IMPORTANCE_DEFAULT;
+
+    public static final String LOCATION_CHANNEL_ID = "location";
+    public static final int LOCATION_IMPORTANCE = NotificationManager.IMPORTANCE_HIGH;
+
+}
--- app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/NotificationSetUp.java
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/NotificationSetUp.java
@@ -1,7 +1,15 @@
 package com.apptec.registrateapp.mainactivity.fnotification;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import com.apptec.registrateapp.App;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 /**
- * This class wil set up the notifications channel
+ * This class wil set up the notifications channels
  */
 public class NotificationSetUp implements Runnable {
 
@@ -14,4 +22,19 @@ public class NotificationSetUp implements Runnable {
         // TODO:
 
     }
+
+
+    private void createNotificationChannel(String channelId, int importance, String name, String description) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(App.getContext(), NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
