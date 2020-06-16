d545d79e53361e6af59793c0d86dc029f2bc9636
renatojobal <renatojobal@gmail.com>
2020-06-15 20:26:17 -0500

    Devolving the NotificationBuilder to handle the notification process

--- app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/FCM.java
+++ app/src/main/java/com/apptec/registrateapp/mainactivity/fnotification/FCM.java
@@ -3,15 +3,10 @@ package com.apptec.registrateapp.mainactivity.fnotification;
 
 import androidx.annotation.NonNull;
 
-import com.apptec.registrateapp.models.NotificationModel;
-import com.apptec.registrateapp.repository.localdatabase.RoomHelper;
 import com.apptec.registrateapp.repository.sharedpreferences.SharedPreferencesHelper;
 import com.apptec.registrateapp.util.Constants;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Date;
-import java.util.Map;
 
 import timber.log.Timber;
 
@@ -31,39 +26,7 @@ public class FCM extends FirebaseMessagingService {
 
         new Thread(new NotificationBuilder(remoteMessage)).start();
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Timber.d("Message data payload: " + remoteMessage.getData().getClass());
-            Timber.d("Message data: " + remoteMessage.getData());
-        }
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Timber.d("Message Notification Body: " + remoteMessage.getNotification().getBody());
-            Timber.d("Notification title: " + remoteMessage.getNotification().getTitle());
-
-            String title = remoteMessage.getNotification().getTitle();
-            String text = remoteMessage.getNotification().getBody();
-            Map map = remoteMessage.getData();
-
-            Timber.d("expirationDate: " + map.get("expirationDate").toString());
-            Timber.d("sentDate" + map.get("sentDate").toString());
-
-
-            Date expirationDate = new Date(Long.parseLong(map.get("expirationDate").toString()) * 1000);
-            Date sentDate = new Date(Long.parseLong(map.get("sentDate").toString()) * 1000);
-
-            NotificationModel notification = new NotificationModel(title, text, expirationDate, sentDate);
-
-
-            /**
-             * Saving it at database
-             */
-            RoomHelper.getAppDatabaseInstance().notificationDao().insert(notification);
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below
     }
 
 
