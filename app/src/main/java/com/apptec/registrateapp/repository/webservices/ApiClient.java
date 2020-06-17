33065dd2b755906b476e75b0c3756345e60aa22b
renatojobal <renatojobal@gmail.com>
2020-06-17 13:22:10 -0500

    Moved the interceptor class to a different file class

--- app/src/main/java/com/apptec/registrateapp/repository/webservices/ApiClient.java
+++ app/src/main/java/com/apptec/registrateapp/repository/webservices/ApiClient.java
@@ -5,57 +5,24 @@ import com.apptec.registrateapp.util.Constants;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import timber.log.Timber;
 
 public class ApiClient {
     /**
-     * ApiClient provides a retrofit instance for make the call to the server
+     * @Override public Response intercept(Chain chain) throws IOException {
+     * return null;
+     * }* ApiClient provides a retrofit instance for make the call to the server
      */
 
     private static Retrofit retrofit = null;
 
 
-    private static Interceptor interceptor = new Interceptor() {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request request = chain.request();
-            okhttp3.Response response = chain.proceed(request);
-
-
-            // Deal with the issues the way we need to
-            if (response.code() == 401) {
-                // Unauthorized
-                // TODO: Ask a new refresh token
-
-            }
-
-
-            int tryCount = 0;
-            while (!response.isSuccessful() && tryCount < 3) {
-
-                Timber.d("Request is not successful" + tryCount);
-
-                tryCount++;
-
-                // retry the request
-                response = chain.proceed(request);
-            }
-
-            // otherwise just pass the original response on
-            return response;
-
-
-        }
-    };
+    private static Interceptor interceptor = new InterceptorImpl();
 
     public static Retrofit getClient() {
         if (retrofit == null) {
