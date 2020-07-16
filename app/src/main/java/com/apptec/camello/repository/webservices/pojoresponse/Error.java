b646de5587983df2632c7460f40f9e2789f52f84
renatojobal <renatojobal@gmail.com>
2020-07-16 18:47:14 -0500

    Fixed start working process

--- app/src/main/java/com/apptec/camello/repository/webservices/pojoresponse/Error.java
+++ app/src/main/java/com/apptec/camello/repository/webservices/pojoresponse/Error.java
@@ -20,5 +20,19 @@ public class Error {
     @SerializedName("code")
     private int code;
 
+    public String getMessage() {
+        return message;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
