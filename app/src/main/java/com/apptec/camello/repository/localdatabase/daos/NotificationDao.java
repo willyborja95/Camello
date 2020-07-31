2c0f8ba875a0bf03af4713ae8a79cc71e058d52a
renatojobal <renatojobal@gmail.com>
2020-07-31 16:44:13 -0500

    Created methods for insert or replace notifications

--- app/src/main/java/com/apptec/camello/repository/localdatabase/daos/NotificationDao.java
+++ app/src/main/java/com/apptec/camello/repository/localdatabase/daos/NotificationDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.apptec.camello.models.NotificationModel;
@@ -22,6 +23,9 @@ public interface NotificationDao {
 
     @Query("SELECT * FROM " + DBConstants.NOTIFICATION_TABLE + " where " + DBConstants.NOTIFICATION_PK + " = :id")
     List<NotificationModel> loadNotificationSync(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertOrReplace(List<NotificationModel> notificationModelList);
 
     @Insert
     void insert(NotificationModel notification);
