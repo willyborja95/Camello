b87be4a2d5859e6ba281e00c66bfc9705f02e547
renatojobal <renatojobal@gmail.com>
2020-08-09 09:55:50 -0500

    Created resources for present the privacy policies at the beginning

---
+++ app/src/main/java/com/apptec/camello/loginactivity/FormFragment.java
@@ -0,0 +1,12 @@
+package com.apptec.camello.loginactivity;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * The principal fragment that shows the form to the user to insert his password and
+ * email.
+ */
+public class FormFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/apptec/camello/loginactivity/ShowPoliciesFragment.java
@@ -0,0 +1,9 @@
+package com.apptec.camello.loginactivity;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * The fragment that show a web view of the privacy policies
+ */
+public class ShowPoliciesFragment extends Fragment {
+}
